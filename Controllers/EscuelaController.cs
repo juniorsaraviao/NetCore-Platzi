cf18426590d50c0f1db68d71a22dc503029c299f
juniorsaraviao <junior.saravia.o@uni.pe>
2022-02-21 23:21:37 -0500

    Creating Index and Controller

---
+++ Controllers/EscuelaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_Platzi.Controllers
+{
+   public class EscuelaController : Controller
+   {
+      public IActionResult Index()
+      {
+         return View();
+      }
+   }
+}
