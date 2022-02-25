7aadbd6257d2ad2edeaa36a4ba8cfbd8b4a8ef08
juniorsaraviao <junior.saravia.o@uni.pe>
2022-02-24 23:09:09 -0500

    Add partial view

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NetCore_Platzi.Models;
 using System;
+using System.Collections.Generic;
 
 namespace NetCore_Platzi.Controllers
 {
@@ -8,16 +9,43 @@ namespace NetCore_Platzi.Controllers
    {
       public IActionResult Index()
       {
-         var asignatura = new Asignatura 
+         var asignatura = new Asignatura
          {
-            UniqueId = Guid.NewGuid().ToString(),
-            Nombre = "Programación"
+            Nombre = "Programación",
+            UniqueId = Guid.NewGuid().ToString()
+         };
+         return View(asignatura);
+      }
+
+      public IActionResult MultiAsignatura()
+      {
+         var listaAsignaturas = new List<Asignatura>() {
+            new Asignatura {
+            Nombre = "Matemáticas",
+            UniqueId = Guid.NewGuid ().ToString ()
+            },
+            new Asignatura {
+            Nombre = "Educación Física",
+            UniqueId = Guid.NewGuid ().ToString ()
+            },
+            new Asignatura {
+            Nombre = "Castellano",
+            UniqueId = Guid.NewGuid ().ToString ()
+            },
+            new Asignatura {
+            Nombre = "Ciencias Naturales",
+            UniqueId = Guid.NewGuid ().ToString ()
+            },
+            new Asignatura {
+            Nombre = "Programación",
+            UniqueId = Guid.NewGuid ().ToString ()
+            }
          };
 
          ViewBag.DynamicThings = "Explorando ASP.NET";
          ViewBag.Date = DateTime.Now;
 
-         return View(asignatura);
+         return View(listaAsignaturas);
       }
    }
 }
