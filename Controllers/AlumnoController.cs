b2049614aeda2e9cc6dea8af18a8f6a1d302f4f5
juniorsaraviao <junior.saravia.o@uni.pe>
2022-02-28 23:58:30 -0500

    Add Alumno Crud

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -14,6 +14,8 @@ namespace NetCore_Platzi.Controllers
          _context = context;
       }
 
+      [Route("Alumno/Index")]
+      [Route("Alumno/Index/{id}")]
       public IActionResult Index(string id)
       {
          if (!string.IsNullOrEmpty(id))
@@ -33,6 +35,53 @@ namespace NetCore_Platzi.Controllers
          ViewBag.Date = DateTime.Now;
 
          return View(_context.Alumnos);
+      }
+
+      [Route("Alumno/Update")]
+      public IActionResult Update(string id)
+      {
+         var curso = _context.Alumnos.FirstOrDefault(x => x.Id == id);
+         return View(curso);
+      }
+
+      [Route("Alumno/Update")]
+      [HttpPost]
+      public IActionResult Update(Alumno alumno)
+      {
+         ViewBag.Fecha = DateTime.Now;
+
+         if (ModelState.IsValid)
+         {
+            var alumnoDb = _context.Alumnos.Where(c => c.Id == alumno.Id).SingleOrDefault();
+            if (alumnoDb == null || string.IsNullOrWhiteSpace(alumnoDb.Id))
+            {
+               ViewBag.MensajeExtra = "El alumno no existe. Compruebe el ID.";
+
+               return View(alumno);
+            }
+            var escuela = _context.Escuelas.FirstOrDefault();
+            if (alumno.Nombre != alumnoDb.Nombre) alumnoDb.Nombre = alumno.Nombre;
+            if (alumno.CursoId != alumnoDb.CursoId) alumnoDb.CursoId = alumno.CursoId;
+
+            _context.Alumnos.Update(alumnoDb);
+            _context.SaveChanges();
+            ViewBag.MensajeExtra = "Alumno actualizado";
+
+            return RedirectToAction("Index", "Alumno", alumno.Id);
+         }
+         else
+         {
+            return View(alumno);
+         }
+      }
+
+      [Route("Alumno/Delete/{id}")]
+      public IActionResult Delete(string id)
+      {
+         var alumno = _context.Alumnos.FirstOrDefault(x => x.Id == id);
+         _context.Alumnos.Remove(alumno);
+         _context.SaveChanges();
+         return RedirectToAction("Index");
       }
    }
 }
