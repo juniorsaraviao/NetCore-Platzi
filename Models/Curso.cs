b2346cd21891c5207a10b1dbce8a54026a13d0be
juniorsaraviao <junior.saravia.o@uni.pe>
2022-03-01 22:43:27 -0500

    Add data annotations

--- Models/Curso.cs
+++ Models/Curso.cs
@@ -3,13 +3,18 @@ using System.ComponentModel.DataAnnotations;
 
 namespace NetCore_Platzi.Models
 {
-   public class Curso:ObjetoEscuelaBase
+   public class Curso : ObjetoEscuelaBase
    {
-      [Required]
+      [Required(ErrorMessage ="El nombre del curso es requerido")]
+      [StringLength(5)]
       public override string Nombre { get; set; }
       public TiposJornada Jornada { get; set; }
       public List<Asignatura> Asignaturas{ get; set; }
       public List<Alumno> Alumnos{ get; set; }
+
+      [Display(Prompt = "Dirección correspondencia", Name = "Address")]
+      [Required(ErrorMessage = "Se requiere incluir una dirección")]
+      [MinLength(10, ErrorMessage = "La longitud mínima de la dirección es 10")]
       public string Dirección { get; set; }
       public string EscuelaId { get; set; }
       public Escuela Escuela { get; set; }
